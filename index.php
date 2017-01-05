c9fb84a8ab24b47c2c073f993b178440751fae8d
ebash0 <ebash.0@yandex.com>
2017-01-05 19:48:14 +0200

    encoding

--- index.php
+++ index.php
@@ -3,16 +3,16 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <title>Сервис сокращения URL</title>
+  <title>РЎРµСЂРІРёСЃ СЃРѕРєСЂР°С‰РµРЅРёСЏ URL</title>
 </head>
 <body>
   <div class="container">
     <div class="login">
-      <h1>Сократить URL</h1>
+      <h1>РЎРѕРєСЂР°С‚РёС‚СЊ URL</h1>
 	  
       <form id="shortener">
         <p><input type="text" name="url" id="longurl"  placeholder="http://google.com"></p>
-        <p class="submit"><input type="submit" value="Сократить!"/></p>
+        <p class="submit"><input type="submit" value="РЎРѕРєСЂР°С‚РёС‚СЊ!"/></p>
       </form>
 	  
 	  <h3 id="response"></h3>
@@ -21,7 +21,6 @@
 	  
     </div>
   </div>
-  
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script>
 $(function () {
